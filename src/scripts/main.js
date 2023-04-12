c93dd7775af39a58c11e042cc447925f8c87f3a9
Luan Glaab <luanf.glaab@gmail.com>
2023-04-12 17:32:53 -0300

    Teste

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+const dataDoEvento = new Date ("Apr 13, 2023 00:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento =  Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento =  Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento =  Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById ('contador').innerHTML = 'Evento Expirado';
+    }
+}, 1000);
